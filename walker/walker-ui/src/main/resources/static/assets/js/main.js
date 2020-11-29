0797ce01c708fa10fcf3ccd077de1543eb90a6bd
Dedtyaryov Pavlo <pashka@ukr.net>
2020-11-29 21:04:49 +0200

    Polish css and js

--- walker/walker-ui/src/main/resources/static/assets/js/main.js
+++ walker/walker-ui/src/main/resources/static/assets/js/main.js
@@ -1,9 +1,3 @@
-/**
-* Template Name: Appland - v2.2.0
-* Template URL: https://bootstrapmade.com/free-bootstrap-app-landing-page-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 !(function($) {
   "use strict";
 
